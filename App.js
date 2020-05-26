4f08398f681392320496806e87dc22dfd13c988e
urosmeh <uros.meh@gmail.com>
2020-05-26 15:15:30 +0200

    flexbox practice

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View,  Button, TextInput} from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState("Initial ...")
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View 
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+      <View style={{
+        backgroundColor: "red",
+        flex: 3,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <Text>1</Text>
+      </View>
+
+      <View style={{
+        backgroundColor: "green", 
+        flex: 2, 
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <Text>2</Text>
+      </View>
+
+      <View style={{
+        backgroundColor: "blue",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
